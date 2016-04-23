78f1f5c4f0a93d2bf8bfbc63b1056271827fa898
cartermp <phillip.carter@outlook.com>
2016-04-22 17:13:13 -0700

    theeeeeemmeememememeemememeemememememee

--- NETCatalog/NETCatalog.Droid/MainActivity.cs
+++ NETCatalog/NETCatalog.Droid/MainActivity.cs
@@ -9,7 +9,11 @@ using Android.OS;
 
 namespace NETCatalog.Droid
 {
-    [Activity(Label = "NETCatalog", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@android:style/Theme.Material.Light", 
+              Label = ".NET Catalog", 
+              Icon = "@drawable/icon", 
+              MainLauncher = true, 
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
