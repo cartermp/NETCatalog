dd6fe5badfb2fc35ba7b9a0b7039a29e0db8cb5e
cartermp <phillip.carter@outlook.com>
2016-06-14 11:04:30 -0700

    iOS working again

--- NETCatalog.App/NETCatalog.App.iOS/AppDelegate.cs
+++ NETCatalog.App/NETCatalog.App.iOS/AppDelegate.cs
@@ -23,7 +23,7 @@ namespace NETCatalog.App.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            LoadApplication(new App());
+            LoadApplication(new NETCatalogApp());
 
             return base.FinishedLaunching(app, options);
         }
